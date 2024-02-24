f980a29451031e59b2754158daf43b8ac43ace2f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-02-24 10:21:25 +0000

    Add basic statistics

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import htm from "htm"
-import{ NavLink, Link, HashRouter, Routes, Route } from "react-router-dom";
+import { NavLink, Link, HashRouter, Routes, Route } from "react-router-dom";
 const html = htm.bind(React.createElement)
-const UnicodeToLaTeX = React.lazy(() => import( './unicode-to-latex.js'));
-const ReplaceText = React.lazy(() => import( './replace-text.js'));
-const DiffText = React.lazy(() => import( './diff-text.js'));
-const SvgToPng = React.lazy(() => import( './svg-to-png.js'));
-const WordCount = React.lazy(() => import( './word-count.js'));
+const UnicodeToLaTeX = React.lazy(() => import('./unicode-to-latex.js'));
+const ReplaceText = React.lazy(() => import('./replace-text.js'));
+const FindDifferences = React.lazy(() => import('./find-differences.js'));
+const SvgToPng = React.lazy(() => import('./svg-to-png.js'));
+const WordCount = React.lazy(() => import('./word-count.js'));
+const BasicStatistics = React.lazy(() => import('./basic-statistics.js'));
 
 
-function menuClassName({isActive}) {
+function menuClassName({ isActive }) {
     return [isActive ? "active" : "", "list-group-item", "list-group-item-action"].join(" ")
 }
 
@@ -26,11 +27,12 @@ function App() {
             <div className="row">
                 <div className="col-sm-2 pt-3">
                     <div className="list-group">
-                        <${NavLink} className=${menuClassName} to="/unicode-to-latex">Unicode To LaTeX<//>
+                        <${NavLink} className=${menuClassName} to="/unicode-to-latex">Unicode to LaTeX<//>
                         <${NavLink} className=${menuClassName} to="/replace-text">Replace Text<//>
-                        <${NavLink} className=${menuClassName} to="/diff-text">Diff Text<//>
-                        <${NavLink} className=${menuClassName} to="/svg-to-png">SVG to PNG<//>
+                        <${NavLink} className=${menuClassName} to="/find-differences">Find Differences<//>
+                        <${NavLink} className=${menuClassName} to="/svg-to-png">Convert SVG to PNG<//>
                         <${NavLink} className=${menuClassName} to="/word-count">Word Count<//>
+                        <${NavLink} className=${menuClassName} to="/basic-statistics">Basic Statistics<//>
                     </div>
                 </div>
                 <div className="col-sm-10 pt-3">
@@ -38,9 +40,10 @@ function App() {
                         <${Routes}>
                             <${Route} path="/unicode-to-latex" element=${html`<${UnicodeToLaTeX} />`} />
                             <${Route} path="/replace-text" element=${html`<${ReplaceText} />`} />
-                            <${Route} path="/diff-text" element=${html`<${DiffText} />`} />
+                            <${Route} path="/find-differences" element=${html`<${FindDifferences} />`} />
                             <${Route} path="/svg-to-png" element=${html`<${SvgToPng} />`} />
                             <${Route} path="/word-count" element=${html`<${WordCount} />`} />
+                            <${Route} path="/basic-statistics" element=${html`<${BasicStatistics} />`} />
                         <//>
                     <//>
                 </div>
---
+++ basic-statistics.js
@@ -0,0 +1,195 @@
+import * as React from 'react';
+import htm from 'htm';
+import Chart from 'react-apexcharts';
+
+const html = htm.bind(React.createElement)
+
+function round(num) {
+    for (let i = 0; i < 256; i++) {
+        if (Math.floor(num * Math.pow(10, i)) !== 0) {
+            return Math.floor(num * Math.pow(10, i+2)) / Math.pow(10, i+2);
+        }
+    }
+}
+
+function mean(nums) {
+    if (nums.length === 0) {
+        return NaN;
+    } else {
+        const mu = nums.reduce((p,c)=>p+c,0)/nums.length;
+        return mu;
+    }
+}
+
+function variance(nums) {
+    if (nums.length === 0) {
+        return NaN;
+    } else {
+        const mu = mean(nums);
+        const sigma2 = nums.reduce((p,c)=>p+Math.pow(c-mu,2),0)/nums.length;
+        return sigma2;
+    }
+}
+
+function standardDeviation(nums) {
+    if (nums.length === 0) {
+        return NaN;
+    } else {
+        const sigma2 = variance(nums);
+        const sigma = Math.sqrt(sigma2);
+        return sigma;
+    }
+}
+
+
+function max(nums) {
+    if (nums.length === 0) {
+        return NaN;
+    } else {
+        const max = Math.max(...nums);
+        return max;
+    }
+}
+
+function min(nums) {
+    if (nums.length === 0) {
+        return NaN;
+    } else {
+        const min = Math.min(...nums);
+        return min;
+    }
+}
+
+function midRange(nums) {
+    if (nums.length === 0) {
+        return NaN;
+    } else {
+        const mid = (min(nums) + max(nums)) / 2;
+        return mid;
+    }
+}
+
+function range(nums) {
+    if (nums.length === 0) {
+        return NaN;
+    } else {
+        const range = max(nums) - min(nums);
+        return range;
+    }
+}
+
+function modes(nums) {
+    if (nums.length === 0) {
+        return [];
+    } else {
+        const counts = nums.reduce((c, n) => (c[n] = (c[n] ?? 0) + 1, c), {});
+        const max = Math.max(...Object.values(counts));
+        const modes = Object.keys(counts).filter(k => counts[k] === max);
+        return modes;
+    }
+}
+
+function median(nums) {
+    if (nums.length === 0) {
+        return NaN;
+    } else if(nums.length % 2 === 1) {
+        const mid = Math.floor(nums.length / 2);
+        const med = nums[mid];
+        return med;
+    } else {
+        const mid = nums.length / 2;
+        const med = (nums[mid - 1] + nums[mid]) / 2;
+        return med;
+    }
+}
+
+function firstQuartile(nums) {
+    if (nums.length === 0) {
+        return NaN;
+    } else {
+        const med = median(nums);
+        const small = nums.filter(x => x < med);
+        const q1 = median(small);
+        return q1
+    }
+}
+
+function thirdQuartile(nums) {
+    if (nums.length === 0) {
+        return NaN;
+    } else {
+        const med = median(nums);
+        const large = nums.filter(x => x > med);
+        const q3 = median(large);
+        return q3
+    }
+}
+
+function hist(nums) {
+    if (nums.length === 0) {
+        return {};
+    } else {
+        const counts = nums.reduce((c, n) => (c[n] = (c[n] ?? 0) + 1, c), {});
+        return counts
+    }
+}
+
+function split(str) {
+    return str.trim().split(/\s+/).map(Number).filter(x => !isNaN(x))
+}
+
+export default function WordCount() {
+    const [state, setState] = React.useState({ value: "" })
+    const handleInput = (event) => setState({ ...state, value: split(event.target.value) })
+    return html`
+        <div>
+            <h2>Basic Statistics</h2>
+            <div className="row">
+                <div className="col-sm-12">
+                    <table className="table">
+                        <tbody>
+                            <tr>
+                                <th>Mean</th>
+                                <td>${round(mean(state.value))}</td>
+                                <th>Variance</th>
+                                <td>${round(variance(state.value))}</td>
+                                <th>Standard deviation</th>
+                                <td>${round(standardDeviation(state.value))}</td>
+                            </tr>
+                            <tr>
+                                <th>First quartile</th>
+                                <td>${firstQuartile(state.value)}</td>
+                                <th>Median/ Second quartile</th>
+                                <td>${median(state.value)}</td>
+                                <th>Third quartile</th>
+                                <td>${thirdQuartile(state.value)}</td>
+                            </tr>
+                            <tr>
+                                <th>Mode</th>
+                                <td>${modes(state.value)?.join(", ")}</td>
+                                <th>Max</th>
+                                <td>${max(state.value)}</td>
+                                <th>Min</th>
+                                <td>${min(state.value)}</td>
+                            </tr>
+                            <tr>
+                                <th>Mid-range</th>
+                                <td>${midRange(state.value)}</td>
+                                <th>Range</th>
+                                <td>${range(state.value)}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-6">
+                    <textarea className="form-control mt-3" onInput=${handleInput} rows="20"></textarea>
+                </div>
+                <div className="col-sm-6">
+                    <${Chart} options=${{xaxis: {categories: Object.keys(hist(state.value))}}} series=${[{name: "Count", data: Object.values(hist(state.value))}]} type="bar" />
+                </div>
+            </div>
+        </div>
+    `
+}

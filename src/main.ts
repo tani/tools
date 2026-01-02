ea33faf5708d396c6d1fd59a6001e947b49b617e
TANIGUCHI Masaya <ta2gch@gmail.com>
2026-01-02 10:14:35 +0900

    fix: import bootstrap js to enable sidebar collapse

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router";
 import "bootswatch/dist/lumen/bootstrap.min.css";
+import "bootstrap";
 import "@git-diff-view/vue/styles/diff-view.css";
 
 createApp(App).use(router).mount("#app");
